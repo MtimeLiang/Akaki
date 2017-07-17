3207b8f4c626921f7b24295d79d4e5f95f71e922
leiliang <liang.lei@mtime.com>
2017-07-17 11:42:43 +0800

    recommit

--- common/token/token.go
+++ common/token/token.go
@@ -1,8 +1,6 @@
 package token
 
 import (
-	"fmt"
-
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
@@ -14,11 +12,8 @@ const (
 // c := jwt.MapClaims{"username": "liang", "exp": time.Now().Add(time.Hour * 72).Unix(),}
 // token, err := New([]byte(SignKey), c)
 func New(signKey []byte, c jwt.MapClaims) (string, error) {
-	// Create the token
 	token := jwt.New(jwt.SigningMethodHS256)
-	// Set some claims
 	token.Claims = c
-	// Sign and get the complete encoded token as a string
 	tokenString, err := token.SignedString(signKey)
 	return tokenString, err
 }
@@ -29,12 +24,9 @@ func Parse(tokenStr, signKey string) (token *jwt.Token, err error) {
 	jwtToken, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
 		return []byte(signKey), nil
 	})
-	fmt.Println("token value is = ", jwtToken)
 	if err == nil && jwtToken.Valid {
-		fmt.Println("Your token is valid.  I like your style.")
 		return jwtToken, nil
 	}
-	fmt.Println("This token is terrible!  I cannot accept this.")
 	return nil, err
 }
 
